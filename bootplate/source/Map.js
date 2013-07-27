2029259eece326625264dd78c7c44cf10a76790a
Cyril Moralès <cyril.morales@epitech.eu>
2013-07-27 19:25:58 +0200

    Ajout des boutons sur la carte

--- bootplate/source/Map.js
+++ bootplate/source/Map.js
@@ -3,6 +3,11 @@ enyo.kind({
 	components: [
 		{kind: "FittableRows", classes: "enyo-fit", components: [
 			{kind: "Map", name: "map", layer: "osm", bingCredentials: "AqwmKr40FdqD4Ntpo_ik3UOKXqG4uT5niPKJDhXkdNJhDqvwyscuJtWhZ72QVWAI", fit: true, onLoaded: "setMap"},
+			{kind: "onyx.Toolbar", components: [
+				{kind: "onyx.Button", content: "+", ontap: "btnZoomIn"},
+				{kind: "onyx.Button", content: "-", ontap: "btnZoomOut"},
+				{kind: "onyx.Button", content: "G", ontap: "btnGeoloc"}
+			]}
 		]}
 	],
 	create: function() {
@@ -12,5 +17,14 @@ enyo.kind({
 	setMap: function() {
 		var bounds = new L.LatLngBounds(new L.LatLng(43.66, 3.78), new L.LatLng(43.55, 3.97));
 		this.$.map.setMaxBounds(bounds);
+	},
+	btnZoomOut: function() {
+		this.$.map.zoomOut();
+	},
+	btnZoomIn: function() {
+		this.$.map.zoomIn();
+	},
+	btnGeoloc: function() {
+		
 	}
 });
